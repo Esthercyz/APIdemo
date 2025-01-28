a175792fefd7be5facbfe8352789cdaafbe9862d
Chen Yuzi <10195101483@stu.ecnu.edu.cn>
2025-01-28 15:01:45 +0800

    dubbo framework integration

--- myapi-gateway/src/main/java/com/cyz/myapigateway/MyapiGatewayApplication.java
+++ myapi-gateway/src/main/java/com/cyz/myapigateway/MyapiGatewayApplication.java
@@ -1,25 +1,25 @@
 package com.cyz.myapigateway;
 
+import com.yupi.springbootinit.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableDubbo
+@Service
 public class MyapiGatewayApplication {
+    @DubboReference
+    private DemoService demoService;
 
     public static void main(String[] args) {
         SpringApplication.run(MyapiGatewayApplication.class, args);
     }
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("path_route", r -> r.path("/hi")
-                        .uri("https://google.com"))
-                .route("host_route", r -> r.path("/jump")
-                        .uri("https://baidu.com"))
-                .build();
-    }
 }
